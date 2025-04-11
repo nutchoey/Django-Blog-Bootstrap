f13d3f0e318285283cf2c0f474d4ec05d9e1f47e
nutchoey <nutchoeyap31@gmail.com>
2025-04-11 14:43:21 +0700

    config login system

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-]
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login')
+,]
