8d73a65be41d3a77f5dc5e59c7fab9b1b1d83240
nutchoey <nutchoeyap31@gmail.com>
2025-04-11 14:05:14 +0700

    config crispy forms

--- DjangoBlogBootstrap/settings.py
+++ DjangoBlogBootstrap/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tinymce',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'users',
     'blog',
 ]
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
